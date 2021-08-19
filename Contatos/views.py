1910352ce6d454532b26df449b7ccb26804d0d6b
danyel-dev <danyelpinheiro154@hotmail.com>
2021-08-19 16:23:59 -0300

    view de detalhes_contato criada

--- Contatos/views.py
+++ Contatos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 
 from .models import Contato
@@ -6,4 +6,9 @@ from .models import Contato
 
 def home(request):
     contatos = Contato.objects.all()
-    return render(request, 'agenda/home.html', {'contatos': contatos})
+    return render(request, 'Contatos/home.html', {'contatos': contatos})
+
+
+def detalhes_contato(request, id_contato):
+    contato = Contato.objects.get(id=id_contato)
+    return render(request, 'Contatos/detalhes_contato.html', {'contato': contato})
