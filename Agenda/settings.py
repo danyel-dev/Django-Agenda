e2c2767c47e1ed07b7d121d40979bb5928b0b647
danyel-dev <danyelpinheiro154@hotmail.com>
2021-08-22 14:05:25 -0300

    app de accounts criado

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Contatos',
+    'Accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Agenda/urls.py
+++ Agenda/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('Accounts.urls')),
     path('', include('Contatos.urls')),
 ]
 
