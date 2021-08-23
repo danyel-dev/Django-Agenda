99fdf46ca0162922ff0433a86ae0c83091107ec8
danyel-dev <danyelpinheiro154@hotmail.com>
2021-08-23 20:33:57 -0300

    login e logout adicionados

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = auth.authenticate(request, username=username, password=password)
+
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos.')
+        return render(request, 'accounts/login.html')
+    
+    auth.login(request, user)
+    messages.success(request, 'Você fez login com sucesso!')
+
+    return redirect('dashboard')
 
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('login')
 
 
 def register(request):
@@ -62,5 +78,6 @@ def register(request):
     return redirect('login')
 
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
--- Contatos/views.py
+++ Contatos/views.py
@@ -4,10 +4,12 @@ from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Contato
 
 
+@login_required(redirect_field_name='login')
 def home(request):
     contatos = Contato.objects.order_by('-id').filter(mostrar_contato=True)
 
